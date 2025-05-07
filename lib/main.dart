1f44ef2b96051346ac918e4219b59742071ed5fc
ukashaanwarpk <anwarukasha0341@gmail.com>
2025-05-07 17:33:52 +0500

    Refactor app structure: enhance splash screen with navigation, add circular icon component, and update color configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_apps_bloc/config/colors/app_colors.dart';
+import 'package:news_apps_bloc/config/routes/route_name.dart';
+import 'package:news_apps_bloc/config/routes/routes.dart';
 import 'package:news_apps_bloc/utils/service_locator.dart';
 import 'package:news_apps_bloc/views/splash/splash_screen.dart';
 
@@ -16,9 +20,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'News App',
       theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.whiteColor,
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.interTextTheme(),
       ),
-      home: SplashScreen(),
+      initialRoute: RouteName.splash,
+      onGenerateRoute: Routes.generateRoute,
     );
   }
 }
